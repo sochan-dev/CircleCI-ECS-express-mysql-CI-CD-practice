1d47f2650d76d00fea1e7e4c793af917efe64cf3
sotatsu kitajima <halosakakitajima@gmail.com>
2022-02-15 20:49:10 +0900

    express,mysql,phpmyadminのコンテナ作成とnext.jsのコンテナと連携

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,30 @@
 import express from 'express';
-
+import mysql from 'mysql2';
+import cors from 'cors';
+const PORT = process.env.PORT;
 const app = express();
-const port = 3000;
+app.use(cors({ origin: '*' }));
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
+const db = mysql.createConnection({
+  host: 'mysql',
+  user: 'root',
+  password: 'password',
+  database: 'initialdb',
+  multipleStatements: true, //複数クエリ発行できる
 });
 
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
+app.get('/', (req, res) => {
+  //res.set({ 'Access-Control-Allow-Origin': '*' });
+  db.connect((err) => {
+    if (err) throw err;
+    db.query('select * from test', (err, result) => {
+      if (err) throw err;
+      console.log(result);
+      res.json(result);
+    });
+  });
+});
+
+app.listen(PORT, () => {
+  console.log(`Example app listening at http://localhost:${PORT}`);
 });
